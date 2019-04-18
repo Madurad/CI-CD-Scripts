530f21bc67fa6660a0981b110cf2f0bff3ec11d9
Madura <madura@Madura-Mac.local>
2019-04-18 10:00:05 +0530

    changed logic

--- trigger-another-job-in-jenkins.sh
+++ trigger-another-job-in-jenkins.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+#================================================================
+# Trigger-Persona-Job-after-Batch-Jobs
+#================================================================
+# Script has been developed to stop EC2 instance once it's not closed to specific time.
+# If the time greater than specified time, Server is NOT stopping.
+# If time is NOT greater than specified time, Server will be stopped
+#
+#================================================================
+# IMPLEMENTATION
+#    version         v0.0.1
+#    author          Madura Dissanayake
+#    date            17-04-2019
+#================================================================
 
-echo "job-C-done" > ~/Downloads/sample-text.out
-#echo "job-D-done" >> ~/Downloads/sample-text.out
+echo "job-D-done" > ~/Downloads/sample-text.out
+echo "job-C-done" >> ~/Downloads/sample-text.out
 
 output=`sed 'N;s/\n/ /' ~/Downloads/sample-text.out`
 echo $output
@@ -9,11 +22,11 @@ echo $output
 while true
 do
     output=`sed 'N;s/\n/ /' ~/Downloads/sample-text.out`
-    if [ "$output" != "job-C-done job-D-done" ] 
+    if [ "$output" = "job-C-done job-D-done" ] || [ "$output" = "job-D-done job-C-done" ]
     then
-    echo "pattern not-matched"
-    else
     echo "pattern matched"
     break
+    else
+    echo "pattern not-matched"
     fi
 done
